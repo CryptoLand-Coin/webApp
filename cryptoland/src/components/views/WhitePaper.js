f567edd22756fc34fdd99e3bf249849fc9070e99
zchristy <zchristy44@gmail.com>
2020-02-06 19:07:44 -0600

    Updating Whitepaper view as directed

--- cryptoland/src/components/views/WhitePaper.js
+++ cryptoland/src/components/views/WhitePaper.js
@@ -5,11 +5,7 @@ import { ViewsContent } from '../../components'
 function WhitePaper() {
 
   return(
-    <ViewsContent>
-      <div className='page-title'>
-        <h1>White Paper</h1>
-      </div>
-
+    <ViewsContent whitepaper={true}>
       <div className='paper-container'>
         <iframe title='whitepaper-iframe' src="https://drive.google.com/file/d/1FxNNYA-sbyPNMtLACzJub_vIJCjbbU2_/preview" width={window.innerWidth} height="100%"></iframe>
       </div>
