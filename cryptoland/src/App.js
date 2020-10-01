e169b4b4f8893fbaea6873c8eb5b13ded6ef2dde
Jay Maas <jaymaas.dev@gmail.com>
2020-10-01 12:52:34 -0600

    App Loading State
    Created app state to hold the value of the app being ready (based upon the large images being fully loaded on the page) and the animation for the fade in/fade out being complete. 
    
    Also created an array that contains all the images that require loading before displaying the app. This will allow us to be able to dynamically change the amount of images/assets we are requiring to load.
    
    In total the application will take at minimum 1.7 seconds to display the main content. The main adjustment for time should be the first timeout set at 1000.

--- cryptoland/src/App.js
+++ cryptoland/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 
 import { AppContainer } from "./styles";
@@ -21,31 +21,83 @@ import {
 } from "./components";
 
 function App() {
-  return (
-    <AppContainer>
-      <Navigation />
-      <Tick />
+  const [loadedImages, setLoadedImages] = useState(0);
+  const [images, setImages] = useState([
+    "Hero_BG.png",
+    "foundation.png",
+    "how_it_works.png",
+    "the_app.png",
+    "the_currency.png",
+    "the_rewards.png",
+  ]);
+  const [appReady, setAppReady] = useState(false);
+  const [fadeCompleted, setFadeCompleted] = useState(false);
 
-      <Switch>
-        <Route exact path="/" component={LandingPage} />
-        {/*<Route path='/whitepaper' component={WhitePaper} />*/}
-        <Route path="/howitworks" component={HowItWorks} />
-        <Route path="/currency" component={Currency} />
-        <Route path="/rewards" component={Rewards} />
-        <Route path="/app" component={TheApp} />
-        <Route path="/foundation" component={Foundation} />
-        {/* <Route path='/team' component={Team} /> */}
-        <Route path="/faqs" component={Faqs} />
-        <Route path="/contactus" component={ContactUs} />
-        <Route path="/privacypolicy" component={PrivacyPolicy} />
-        <Route path="/termsandconditions" component={TermsAndConditions} />
+  useEffect(() => {
+    document.getElementById("root").classList.toggle("fade-root");
+    document.getElementById("root").classList.toggle("fade-in");
+    console.log(appReady);
+    function PreloadImage(src, number) {
+      var img = new Image();
+      img.onload = function () {
+        setLoadedImages(number);
+      };
+      img.src = src;
+    }
+    images.map((image, i) => {
+      const adjustedIndex = i + 1;
+      PreloadImage(`assets/images/${image}`, adjustedIndex);
+      if (adjustedIndex === images.length) {
+        setTimeout(() => {
+          document.getElementById("root").classList.toggle("fade-in");
+          document.getElementById("root").classList.toggle("fade-out");
+          setTimeout(() => {
+            setAppReady(true);
+            document.getElementById("root").classList.toggle("fade-out");
+            setTimeout(() => {
+              setFadeCompleted(true);
+              document.getElementById("root").classList.toggle("fade-in");
+            }, 200);
+          }, 500);
+        }, 1000);
+      }
+    });
+  }, []);
 
-        <Route component={NoMatch} />
-      </Switch>
-
-      <Footer />
-    </AppContainer>
-  );
+  if (!appReady || !fadeCompleted) {
+    return (
+      <div className="loading-container">
+        <img
+          src="assets/images/CryptoLand_Logo_White_Small.png"
+          className="App-logo"
+          alt="logo"
+        />
+      </div>
+    );
+  } else {
+    return (
+      <AppContainer>
+        <Navigation />
+        <Tick />
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          {/* <Route path="/whitepaper" component={WhitePaper} /> */}
+          <Route path="/howitworks" component={HowItWorks} />
+          <Route path="/currency" component={Currency} />
+          <Route path="/rewards" component={Rewards} />
+          <Route path="/app" component={TheApp} />
+          <Route path="/foundation" component={Foundation} />
+          {/* <Route path="/team" component={Team} /> */}
+          <Route path="/faqs" component={Faqs} />
+          <Route path="/contactus" component={ContactUs} />
+          <Route path="/privacypolicy" component={PrivacyPolicy} />
+          <Route path="/termsandconditions" component={TermsAndConditions} />
+          <Route component={NoMatch} />
+        </Switch>
+        <Footer />
+      </AppContainer>
+    );
+  }
 }
 
 export default App;
