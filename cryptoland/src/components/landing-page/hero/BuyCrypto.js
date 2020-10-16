e010ecb66e42cd6f35f763dbe69a47c85e172212
Jay Maas <jaymaas.dev@gmail.com>
2020-10-16 11:13:58 -0600

    Removed uncalled methods

--- cryptoland/src/components/landing-page/hero/BuyCrypto.js
+++ cryptoland/src/components/landing-page/hero/BuyCrypto.js
@@ -1,12 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { connect } from "react-redux";
-import { Link } from "react-router-dom";
 
 import { BuyCryptoContainer } from "../../../styles";
 
-import { Button } from "../../../components";
-
-function CryptoProgress(props) {
+function CryptoProgress() {
   return (
     <BuyCryptoContainer>
       <h1>BUY ON</h1>
@@ -14,18 +11,21 @@ function CryptoProgress(props) {
         style={{ marginTop: "2.5%" }}
         href="https://stellarport.io/exchange/GCBIJZQLTEVI6ZZYM4UH2LCUHNVNWUTZMQNOQTZWMOEAGDRWSWFDP2S3/CRYPTO/native/XLM"
         target="_blank"
+        rel="noopener noreferrer"
       >
-        <img src="assets/images/stellarport.png" />
+        <img src="assets/images/stellarport.png" alt="stellarport" />
       </a>
       <a
         href="https://lobstr.co/trade/native/CRYPTO:GCBIJZQLTEVI6ZZYM4UH2LCUHNVNWUTZMQNOQTZWMOEAGDRWSWFDP2S3"
         target="_blank"
+        rel="noopener noreferrer"
       >
-        <img src="assets/images/lobstr.svg" />
+        <img src="assets/images/lobstr.svg" alt="lobstr" />
       </a>
       <a
         href="https://stellarterm.com/exchange/XLM-native/CRYPTO-GCBIJZQLTEVI6ZZYM4UH2LCUHNVNWUTZMQNOQTZWMOEAGDRWSWFDP2S3"
         target="_blank"
+        rel="noopener noreferrer"
       >
         <h4>StellarTerm</h4>
       </a>
